33508136d8661aedee1a383f56a92a305c6f451d
abhay-joshi <abhay.joshi@drillinginfo.com>
2017-03-03 12:55:10 +0530

    added logic to add employee loading logic on app load

--- routing/routing-events-navigation/src/js/components/App.js
+++ routing/routing-events-navigation/src/js/components/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import NavLink from './NavLink.js';
+import { connect } from 'react-redux';
+import { loadEmployees } from '../actions/employees.js';
 
-export default class App extends React.Component {
+class App extends React.Component {
+    componentDidMount() {
+        this.props.loadEmployees();
+    }
+
     render() {
         return (
             <div>
@@ -25,3 +31,8 @@ export default class App extends React.Component {
         )
     }
 }
+
+export default connect(
+    null,
+    { loadEmployees }
+)(App);
