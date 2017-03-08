afcf97c5658516202eb991b311a610afb782fec9
abhay-joshi <abhay.joshi@drillinginfo.com>
2017-03-08 11:37:17 +0530

    added more nesting to about component

--- styling/react-sass-basics/src/js/components/about.js
+++ styling/react-sass-basics/src/js/components/about.js
@@ -3,5 +3,11 @@ import React from 'react';
 export default () => (
     <div className='about container-color'>
         this is about styling!!
+        <div className='inside-container'>
+            inside container
+            <div className='innermost-container'>
+                inner most content!! 
+            </div>
+        </div>
     </div>
 )
